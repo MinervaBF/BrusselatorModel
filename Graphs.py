7757c57e3fbfdc74fee24111b5aa70dae266fe36
MinervaBF <126017587+MinervaBF@users.noreply.github.com>
2025-06-17 14:06:17 +0200

    Update Graphs.py

--- Graphs.py
+++ Graphs.py
@@ -1,3 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#parameters
+a=2
+d_X=1
+d_Y=2
+eta=np.sqrt(d_X/d_Y)
+eta_0=(np.sqrt(1+a*a)-1)/a
+
+b_T=(1+a*eta)**2
+b_H=1+a*a
+q_T=np.sqrt(a*eta/d_X)
+q_H=0
+b_c=b_T if b_T<b_H else b_H
+q_c=q_T if b_T<b_H else q_H
+
+save_to_file=False
+instability="Turing" if b_T<b_H else "Hopf"
+filename=f"brusselator_{instability}_{b_c:.2f}_{a}_{d_X}_{d_Y}"
+
+print(eta,eta_0,b_c,q_c,b_T,b_H,q_T,q_H)
+
+#functions
+def tr(b,q):
+    return b-1-a*a-q*q*(d_X*d_X+d_Y*d_Y)
+
+def det(b,q):
+    return (b-1-q*q*d_X)*(-a*a-q*q*d_Y)+a*a*b
+
+def Delta(b,q):
+    return np.lib.scimath.sqrt(tr(b,q)*tr(b,q)-4*det(b,q)) #it can return complex values unlike np.sqrt()
+
+def sigma_p(b,q):
+    return 0.5*(tr(b,q)+Delta(b,q))
+
+def sigma_m(b,q):
+    return 0.5*(tr(b,q)-Delta(b,q))
+
+def f(b, q):
+    return np.maximum(np.real(sigma_p(b,q)),np.real(sigma_m(b,q)))
+
+
 q = np.linspace(0, 3, 500)
 
 fig, ax = plt.subplots()
@@ -33,5 +76,6 @@ if save_to_file:
     plt.savefig(f"{filename}.png")
 else:
     plt.show()
+
 
 
